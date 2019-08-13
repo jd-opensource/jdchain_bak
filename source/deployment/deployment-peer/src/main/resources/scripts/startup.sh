858f3d565f5b14cef44431ccaba283314888d427
zhaoguangwei <zhaoguangwei@jd.com>
2019-08-13 09:30:39 +0800

    -Xmx1g -Xms512m

--- source/deployment/deployment-peer/src/main/resources/scripts/startup.sh
+++ source/deployment/deployment-peer/src/main/resources/scripts/startup.sh
@@ -5,5 +5,5 @@ PEER=$(ls $HOME/system | grep deployment-peer-)
 if [ ! -n "$PEER" ]; then
   echo "Peer Is Null !!!"
 else
-  nohup java -jar -server -Xmx2g -Xms2g -Dpeer.log=$HOME $HOME/system/$PEER -home=$HOME -c $HOME/config/ledger-binding.conf -p 7080 $* >$HOME/bin/peer.out 2>&1 &
+  nohup java -jar -server -Xmx1g -Xms512m -Dpeer.log=$HOME $HOME/system/$PEER -home=$HOME -c $HOME/config/ledger-binding.conf -p 7080 $* >$HOME/bin/peer.out 2>&1 &
 fi
